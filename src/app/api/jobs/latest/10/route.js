e3d3f07ac3d7b2e3715b52e2eae1af3bb488bbec
SulemanMughal <sulemanmughal155@gmail.com>
2023-08-06 02:50:05 -0700

    bug for jobs

--- src/app/api/jobs/latest/10/route.js
+++ src/app/api/jobs/latest/10/route.js
@@ -6,13 +6,14 @@ import axios from 'axios';
 
 export async function GET(request ){
    try{
-    const response = await axios.get(`${process.env.API_KEY_URL}/jobs`, {
-        headers: {
-            'X-API-Key': process.env.API_KEY
-        }
-        });
-        // console.debug(response.data.jobs)
-        return new Response(JSON.stringify(response.data.jobs.slice(0, 10)))
+    // const response = await axios.get(`${process.env.API_KEY_URL}/jobs`, {
+    //     headers: {
+    //         'X-API-Key': process.env.API_KEY
+    //     }
+    //     });
+    //     // console.debug(response.data.jobs)
+    //     return new Response(JSON.stringify(response.data.jobs.slice(0, 10)))
+    return new Response(JSON.stringify({status : true}))
    } catch(error){
     console.debug(error)
     return new Response(JSON.stringify({status : false}))
