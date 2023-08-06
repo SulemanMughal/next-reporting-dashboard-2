b8b7e7b65611918e53f314587c9cb8a2494b2893
SulemanMughal <sulemanmughal155@gmail.com>
2023-08-06 02:47:37 -0700

    Removed Jobs API

--- src/app/api/jobs/[id]/route.js
+++ src/app/api/jobs/[id]/route.js
@@ -6,13 +6,15 @@ import axios from 'axios';
 
 export async function GET(request, {params}){
     try {
-        const response = await axios.get(`${process.env.API_KEY_URL}/jobs/${params.id}`, {
-            headers: {
-                'X-API-Key': process.env.API_KEY
-            }
-            });
-            return new Response(JSON.stringify({status : response.data.status}))
+        // const response = await axios.get(`${process.env.API_KEY_URL}/jobs/${params.id}`, {
+        //     headers: {
+        //         'X-API-Key': process.env.API_KEY
+        //     }
+        //     });
+        //     return new Response(JSON.stringify({status : response.data.status}))
+        return new Response(JSON.stringify({status : true}))
     } catch (error) {
         console.debug(error)
+        return new Response(JSON.stringify({status : true}))
     }
 }
--- src/app/api/jobs/route.js
+++ src/app/api/jobs/route.js
@@ -6,14 +6,15 @@ import axios from 'axios';
 
 export async function GET(request ){
     try{
-        const response = await axios.get(`${process.env.API_KEY_URL}/jobs`, {
-            headers: {
-                'X-API-Key': process.env.API_KEY
-            }
-            });
-            return new Response(JSON.stringify(response.data))
+        // const response = await axios.get(`${process.env.API_KEY_URL}/jobs`, {
+        //     headers: {
+        //         'X-API-Key': process.env.API_KEY
+        //     }
+        //     });
+        //     return new Response(JSON.stringify(response.data))
+        return new Response(JSON.stringify({status : true}))
     } catch(error){
         console.debug(error)
-        return new Response(JSON.stringify({status : true}))
+        return new Response(JSON.stringify({status : false}))
     }
 }
