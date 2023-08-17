3dbfdeee7dcf4395c212167106a763feba927285
SulemanMughal <sulemanmughal155@gmail.com>
2023-08-17 21:41:18 +0500

    Dashboard : Team Counter

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -9,6 +9,10 @@ import { VscTerminalBash } from "react-icons/vsc"
 import  { MdQuiz  } from "react-icons/md"
 
 
+
+import { GrShieldSecurity } from "react-icons/gr";
+
+
 const admin = [
     [
         "/admin/dashboard",
@@ -48,11 +52,13 @@ function AdminSideBar(){
                 <div className="flex   flex-col items-center ">
                 <Link href="/" className="flex items-center w-full justify-center" >
                     <Image
-                        src="/assets/img/logo.svg"
+                        src="/assets/img/logo_new.png"
                         width={50}
                         height={50}
                         alt="Picture of the author"
+                        
                     />
+                    
                 </Link>
                     <span className="border-none  border-b-[1px] border-gray-200 w-full p-2"></span>
                     {admin.map((item, index) => (
@@ -75,7 +81,7 @@ function UserSideBar(){
             <div className="flex   flex-col items-center ">
             <Link href="/" className="flex items-center w-full justify-center" >
                 <Image
-                    src="/assets/img/logo.svg"
+                    src="/assets/img/logo_new.png"
                     width={50}
                     height={50}
                     
--- src/app/components/admin/dashboard/TeamCounter.jsx
+++ src/app/components/admin/dashboard/TeamCounter.jsx
@@ -22,7 +22,7 @@ export default function TeamCounter(){
         .catch(error => {
             console.error(error);
         });
-    }, []);
+    }, [teamCounter]);
 
     return (
         <>
