955ba1af6df0c05e3366e831744c3e719a35ad47
SulemanMughal <sulemanmughal155@gmail.com>
2023-08-30 00:39:35 +0500

    Admin Dashboard Log COunter Bug FIxed

--- src/app/components/admin/dashboard/LogsCounter.jsx
+++ src/app/components/admin/dashboard/LogsCounter.jsx
@@ -89,7 +89,7 @@ export default function LogsCounter({total_logs}){
             
 
             {
-                logs && (
+                logCounter && (
                     <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105  duration-300">
                         <div className="   w-full col-span-1 relative  m-auto p-0 border-none rounded-lg report-box  "  data-aos="fade-down" data-aos-duration="500" data-aos-delay="600" >
                             <div  className="   block  p-6 bg-deep-blue-violet  rounded-lg shadow relative  ">
